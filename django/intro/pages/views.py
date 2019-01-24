842e20c6fe21344a433de3d433d440f214fb1fc6
hyunwoo Song <gerrar486@gmail.com>
2019-01-24 08:47:49 +0000

    D17 | 190124 | Django

--- django/intro/pages/views.py
+++ django/intro/pages/views.py
@@ -1,6 +1,37 @@
 from django.shortcuts import render
+import random
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+# Template variable
+def dinner(request):
+    menu = ['족발', '햄버거', '치킨', '초밥']
+    pick = random.choice(menu)
+    return render(request, 'dinner.html', {'dinner': pick})
     
+# Variable routing
+def hello(request, name):
+    return render(request, 'hello.html', {'name': name})
+
+def TOT(request):
+    EPL = ['첼시', '토트넘', '맨유', '맨체스터시티', '헐시티']
+    pick = random.choice(EPL)
+    return render(request, 'TOT.html', {'TOT':pick})
+
+# Form tag
+def throw(request):
+    return render(request, 'throw.html')
+    
+def catch(request):
+    message = request.GET.get('message')
+    return render(request, 'catch.html', {'message': message})
+
+# Form 외부로 요청
+def naver(request):
+    return render(request, 'naver.html')
+    
+#Bootstrap
+def bootstrap(request):
+    return render(request, 'bootstrap.html')
